7d8b5ae8d02981e3bfc0c04be7501c9c2c4009a1
Erickson Silva <erickson.lucas@live.com>
2018-01-14 19:08:47 -0200

    Detecting Where Code is Running

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+
+
+AMovingPlatform::AMovingPlatform() 
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+}
+
+
+void AMovingPlatform::Tick(float deltaTime) 
+{
+	Super::Tick(deltaTime);
+
+	if (HasAuthority()) 
+	{
+		FVector location = GetActorLocation();
+		location += FVector(speed * deltaTime, 0, 0);
+		SetActorLocation(location);
+	}
+}
