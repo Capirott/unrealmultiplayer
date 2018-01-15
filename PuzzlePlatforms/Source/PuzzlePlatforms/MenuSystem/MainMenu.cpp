040d5a19c89e408c5a1e83d2e057587b0db9640a
Erickson Silva <erickson.lucas@live.com>
2018-01-15 16:53:44 -0200

    31. Initialisers and UButton Callbacks

--- PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
@@ -1,7 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "MainMenu.h"
 
+#include "Button.h"
 
+bool UMainMenu::Initialize()
+{
+	bool Success = Super::Initialize();
+	if (!Success)
+	{
+		return false;
+	}
 
+	if (!ensure(Host != nullptr)) return false;
+	if (!ensure(Join != nullptr)) return false;
 
+	Host->OnClicked.AddDynamic(this, &UMainMenu::HostServer);
+
+	return true;
+}
+
+void UMainMenu::HostServer()
+{
+	UE_LOG(LogTemp, Warning, TEXT("I'm gonna host server"));
+}
