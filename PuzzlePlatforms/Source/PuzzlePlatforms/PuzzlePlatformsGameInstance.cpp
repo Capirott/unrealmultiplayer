ec59939022300d2f5b5a7e6feb0d582b93509d25
Erickson Silva <erickson.lucas@live.com>
2018-01-15 13:20:16 -0200

    18. Hosting Servers With ServerTravel

--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -17,6 +17,10 @@ void UPuzzlePlatformsGameInstance::Host()
 	UEngine *Engine = GetEngine();
 	if (!ensure(Engine != nullptr)) return;
 	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString& Address)
@@ -24,5 +28,7 @@ void UPuzzlePlatformsGameInstance::Join(const FString& Address)
 	UEngine *Engine = GetEngine();
 	if (!ensure(Engine != nullptr)) return;
 	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
-	//FString::Printf(address);
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+	World->ServerTravel(Address);
 }
